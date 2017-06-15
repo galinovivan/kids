ae564785d2545020dd4e66d87b34260dcf4e9031
galinovivan <9225913@gmail.com>
2017-06-15 16:43:38 +0300

    ki

--- content-base.php
+++ content-base.php
@@ -6,16 +6,24 @@
  * Time: 16:34
  */
 ?>
-<main class="content gray_base_background">
+<main class="content gray_base_background
+<?php is_front_page() ? print 'font_page' : print 'secondary_page'?>">
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <div class="page_thumbnail">
-                <?php the_post_thumbnail(); ?>
-            </div>
+            <?php if (!is_front_page()):?>
             <div class="page_heading">
                 <h1 class="page_title"><?=get_the_title();?></h1>
             </div>
+            <?php endif; ?>
+            <div class="page_thumbnail">
+                <?php the_post_thumbnail(); ?>
+            </div>
+            <?php if (is_front_page()): ?>
+            <div class="page_heading">
+                <h1 class="page_title"><?=get_the_title();?></h1>
+            </div>
+            <?php endif; ?>
             <div class="page_content">
                 <?php the_content(); ?>
             </div>
--- methods.php
+++ methods.php
@@ -6,3 +6,21 @@
  * Time: 17:53
  * Template name: Методики
  */
+?>
+
+
+
+<?php get_header(); ?>
+
+    <main>
+        <?php
+
+        while(have_posts()) {
+            the_post();
+            get_template_part('content', 'base');
+        }
+
+        ?>
+    </main>
+
+<?php get_footer(); ?>
